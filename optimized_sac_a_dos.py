65e69f9824da34ac38102def6a108e222eca4d20
Reda Abdi <redaabdi33@gmail.com>
2025-12-19 17:01:15 +0100

    fonction afin de formater les nombres et les arrondir pour l'affichage, selon leur nature

--- optimized_sac_a_dos.py
+++ optimized_sac_a_dos.py
@@ -7,25 +7,28 @@ class Action:
         """ 
         Args :
             name : Name of the action (Action-1, etc...) 
-            cost : Represent the cost of the action in centimes
-            benefits_pourcent : Represent the pourcent of benefits after 2 years
-            benefits_centimes : Use benefits_pourcent and cost to calculate and represent the value in euros of benefits after 2 years
+            cost : Cost of the action in euros
+            benefits_pourcent : Pourcent of benefits after 2 years
+            benefits_euros : Benefits in euros after 2 years
         """
         self.name = name
-        self.cost = int(cost)
+        self.cost = float(cost)
         self.benefits_pourcent = float(benefits_pourcent.replace("%", ""))
+        self.benefits_euros = self.benefits_pourcent * self.cost / 100
+
 
     @property
-    def cost_centimes(self) :
-        return int(self.cost*10)
-        
+    def units(self):
+        return int(self.cost*100)
+
     @property
-    def benefits_centimes(self):
-        return int(self.benefits_pourcent * self.cost_centimes / 100)
-        
-    @property
-    def benefits_euros(self):
-        return round(float(self.benefits_pourcent * self.cost / 100), 2)
+    def benefits_units(self):
+        return self.benefits_pourcent * self.units / 100
+    
+    @classmethod
+    def format_number(self, number):
+        """Return number without .0 if it's integer"""
+        return int(number) if number.is_integer() else round(number, 2)
 
 
 def get_all_actions():
@@ -40,42 +43,47 @@ def get_all_actions():
         for row in csv_reader :
             name = row[0]
             cost = row[1]
-            benefits = row[2]
+            benefits_pourcent = row[2]
             if float(cost) > 0 :
-                all_actions.append(Action(name, cost, benefits))
+                all_actions.append(Action(name, cost, benefits_pourcent))
 
     return all_actions
 
 def get_best_combo(budget_euros, all_actions) :
-    budget_centimes = budget_euros*10
-    matrice = [[0 for x in range(budget_centimes + 1)] for x in range(len(all_actions) + 1)]
+    budget_units = budget_euros * 100
+    matrice = [[0 for x in range(budget_units + 1)] for x in range(len(all_actions) + 1)]
 
     for action_index in range(1, len(all_actions) + 1):
-        for actual_budget in range(1, budget_centimes + 1) :
-            if all_actions[action_index-1].cost_centimes <= actual_budget :
+        action_unit = all_actions[action_index-1].units
+        action_benefits_units = all_actions[action_index-1].benefits_units
+    
+        for actual_budget in range(1, budget_units + 1) :
+            if action_unit <= actual_budget :
                 matrice[action_index][actual_budget] = max(
                                                             matrice[action_index-1][actual_budget],
-                                                            all_actions[action_index-1].benefits_centimes + matrice[action_index-1][actual_budget - all_actions[action_index-1].cost_centimes]
-                                                        )
+                                                            action_benefits_units + matrice[action_index-1][actual_budget - action_unit]
+                                                            )
             else :
                 matrice[action_index][actual_budget] = matrice[action_index-1][actual_budget]
 
-    actual_budget = budget_centimes
+    actual_budget = budget_units
     action_index = len(all_actions)
     best_combo = {
         "actions_list" : [],
         "total_cost" : 0,
-        "total_benefits" : matrice[-1][-1]/10
+        "total_benefits" : 0
         }
 
     while actual_budget >= 0 and action_index >= 0 :
         action = all_actions[action_index-1]
-        if matrice[action_index][actual_budget] == action.benefits_centimes + matrice[action_index-1][actual_budget - action.cost_centimes] :
+        if matrice[action_index][actual_budget] == action.benefits_units + matrice[action_index-1][actual_budget - action.units] :
             best_combo["actions_list"].append(action)
             best_combo["total_cost"] += action.cost
-            actual_budget -= action.cost_centimes
-        
+            actual_budget -= action.units
         action_index -= 1
+
+    for action in best_combo["actions_list"] :
+        best_combo["total_benefits"] += action.benefits_euros
 
     return best_combo
 
@@ -86,13 +94,12 @@ def view_best_combo(best_combo):
     for action in best_combo["actions_list"] :
         print(
             f"\n> {action.name} :"
-            f"\n  Cost : {action.cost}€"
-            f"\n  Benefits after 2 years : {action.benefits_euros}€ ({action.benefits_pourcent}%)"
+            f"\n  Cost : {Action.format_number(action.cost)}€"
+            f"\n  Benefits after 2 years : {Action.format_number(action.benefits_euros)}€ ({Action.format_number(action.benefits_pourcent)}%)"
               )
         
-    
-    print(f"\nTotal cost : {round(best_combo["total_cost"], 2)}€")
-    print(f"Total benefits (after 2 years) : {round(best_combo["total_benefits"], 2)}€")
+    print(f"\nTotal cost : {Action.format_number(best_combo["total_cost"])}€")
+    print(f"Total benefits (after 2 years) : {Action.format_number(best_combo["total_benefits"])}€")
     
 
 def timer(func):
--- test.py
+++
@@ -1,6 +0,0 @@
-all_actions = ["Trop", "test", "mclaren"]
-all_brands = ["abu djabi", "bahrein"]
-print(35.85*100)
-print(round(35.85*100))
-
-        
