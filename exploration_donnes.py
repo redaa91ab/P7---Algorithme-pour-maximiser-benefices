2ea5447dbbc603d7f101fa3a359a7263523d8e89
redaa91ab <75947968+redaa91ab@users.noreply.github.com>
2026-03-02 14:36:27 +0000

    ajout exploration des donnees code

--- exploration_donnes.py
+++ exploration_donnes.py
@@ -1,9 +1,33 @@
 import pandas as pandas
 
-data = pandas.read_csv("dataset1.csv")
+data = pandas.read_csv("dataset2.csv")
+#print(data)
 
-data_sorted = data.sort_values(by="price", ascending=False)
+data_clean = data[(data["price"] > 0) & (data["profit"] > 0)]
 
 data_trash =  data[data["price"] <= 0]
+#print(data_trash, "\n", len(data_trash), " rows")
 
-print(data_trash, , "\n", len(data_trash), " rows")
+price_data_sorted = data_clean.sort_values(by="price", ascending=False)
+#print(price_data_sorted)
+
+moyenne_price = data_clean["price"].mean()
+#print(moyenne_price)
+
+mediane_price = data_clean["price"].median()
+#print(mediane_price)
+
+ecart_type_price = data_clean["price"].std()
+#print(ecart_type_price)
+
+profit_data_sorted = data_clean.sort_values(by="profit", ascending=False)
+#print(profit_data_sorted)
+
+moyenne_profit = data_clean["profit"].mean()
+#print(moyenne_profit)
+
+mediane_price = data_clean["profit"].median()
+#print(mediane_price)
+
+ecart_type_profit = data_clean["profit"].std()
+print(ecart_type_profit)
